bfe3277bac2fb3ff66ef79b65869ffaed13f4ec4
Leao9203 <45661339+Leao9203@users.noreply.github.com>
2020-04-02 20:18:39 +0800

    安装BBR最新内核

---
+++ install-bbr-centos7.sh
@@ -0,0 +1,5 @@
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+yum --enablerepo=elrepo-kernel install kernel-ml -y
+egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
+grub2-set-default 0
